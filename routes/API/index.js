320a2a9bd2cb551a0d9edca7b3913de2157b9278
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-04-05 15:35:42 -0500

    fixing routes/api/index.js

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const apiRoutes = require('./API');
 
-router.use('/api/users', userRoutes);
+router.use('/api/', apiRoutes);
 
-router.use('/api/thoughts', thoughtRoutes);
+router.use((req, res) => {
+    return res.send("Wrong Route!");
+});
 
 module.exports = router;
